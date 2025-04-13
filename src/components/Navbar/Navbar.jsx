e0fafb55806051bb858644c7447a6f03724794ef
glealnunes <gpfilho2010@gmail.com>
2025-04-12 21:38:35 -0300

    feat: adding navigation bar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./navbar.css"
+
+export default function Navbar() {
+    return(
+        <nav className="navbar">
+            <span>React Toll Project Management</span>
+        </nav>
+    );
+    
+}
