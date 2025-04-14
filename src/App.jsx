0b832c5f015a7384e327721806838d9f9033b6fd
glealnunes <gpfilho2010@gmail.com>
2025-04-13 21:28:23 -0300

    feat: Adding to-do list button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
+import React, { useState } from 'react';
 import './App.css'
 
 import Navbar from './components/Navbar/Navbar';
 import TaskList from './components/TaskList/TaskList'
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    console.log("Função sendo chamada em app")
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
   return (
     <>
       <div className="App">
         <Navbar/>
-        <div>
-          <TaskList title="Pendente"/>
-          <TaskList title="Fazendo"/>
-          <TaskList title="Completa"/>
+        <div className="container">
+          <TaskList title="Pending" onAddTask={addTask} tasks={tasks} />
         </div>
       </div>
     </>
   )
 }
+
+const task = {
+  id:0,
+  title: "new task",
+  state: "pending"
+};
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
 
 export default App
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import './tasklist.css'
 
-export default function TaskList({title}) {
+export default function TaskList({title, onAddTask, tasks}) {
+    const addTask = () => {
+        console.log("chamada dentro do TaskList");
+        onAddTask("New task", "Pending")
+    }
     return(
         <div className="tasklist">
            <div className="title">{title}</div>
-           <div className="content"></div>
+           <div className="content">
+            {tasks.map((task) => {
+                return <div key={task.id}>{task.title}</div>
+            })}
+           </div>
+           <button onClick={addTask}>Add task</button>
         </div>
     )
 }
